2e8f5988f4167e1b0198735f4b697e3d07db2b97
eyoxiao <63781784@qq.com>
2016-04-08 17:14:39 +0800

    add yield generator

--- python/com/terry/generator.py
+++ python/com/terry/generator.py
@@ -7,9 +7,53 @@ Created on Fri Apr 08 15:31:42 2016
 
 def getFirstNFibonacci(number):
     n,a,b = 0,0,1
+    fArray = list()
     while n<number:
-        print b
+        fArray.append(b)
         a,b=b,a+b
         n=n+1
+    return fArray
+    
+def getFibByGen(number):
+    n,a,b=0,0,1
+    while n<number:
+        yield b
+        a,b=b,a+b
+        n=n+1
+
+class Fibonacci(object):
+    def __init__(self,max):
+        self.max = max
+        self.n,self.a,self.b=0,0,1
+    
+    def next(self):
+        if(self.n < self.max):
+            r=self.b
+            self.a,self.b = self.b,self.a+self.b
+            self.n=self.n+1
+            return r
+        raise StopIteration()
         
-getFirstNFibonacci(5)
+    def __iter__(self):
+        return self
+
+f= Fibonacci(10)
+for number in f:
+    print number
+
+for num in getFibByGen(10):
+    print num
+
+#print getFirstNFibonacci(5)
+
+
+
+
+
+
+
+
+
+
+
+
